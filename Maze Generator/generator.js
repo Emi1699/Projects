623ce8c9410551ff9ea50435aefae3a5e3fec8f2
Emi1699 <emisv_16@yahoo.com>
2022-07-22 16:30:37 +0300

    overnight changes - doing some testing

--- Maze Generator/generator.js
+++ Maze Generator/generator.js
@@ -3,26 +3,34 @@ function setup() {
 
   console.log(height)
   console.log(width)
+
+  console.log("QWerqwer")
 }
 
-function draw() {
-  background(25);
+let verr = 3
 
+function draw() {
+  background(25); // 0 = black, 255 = white
+
+  // global variables
+  cellWidth = 25
   stroke('yellow');
   strokeWeight(1.25)
 
-
   // draw vertical walls
-  for (i = 0; i <= width; i+= 25) {
+  for (i = 0; i <= width; i+= cellWidth) {
     line(i, 0, i, height)
   }
 
   // draw horizontal walls
-  for (j = 0; j <= width; j+= 25) {
+
+  for (j = 0; j <= width; j+= cellWidth) {
     line(0, j, width, j)
   }
 
 }
+
+cells = []
 
 class Cell{
   constructor(width, height){
@@ -33,10 +41,4 @@ class Cell{
   draw() {
 
   }
-}
-
-function grid(x1, y1, x2, y2) {
-  stroke('yellow');
-  strokeWeight(1)
-  line(10,10, 100,100);
 }
