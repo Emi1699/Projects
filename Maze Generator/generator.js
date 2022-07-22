0667d7bd3b5a1d331909ff3bb24d6fc6dc44c530
Emi1699 <emisv_16@yahoo.com>
2022-07-22 20:17:37 +0300

    finished cell generation process; need to fix 'arguments.includes()' error in cell constructor

--- Maze Generator/generator.js
+++ Maze Generator/generator.js
@@ -4,15 +4,59 @@ function setup() {
   console.log(width);
   console.log(height);
 
+  finalY = height / cellSize;
+  finalX = width / cellSize;
+
    // create the cells in the maze
-  for (x = 1; x<= width / cellSize; x++) {
-    for (y = 1; y<= height / cellSize; y++) {
+  for (x = 1; x<= finalX; x++) {
+    for (y = 1; y<= finalY; y++) {
       id = x + (width * (y - 1))
 
-      
-    }
-  }
+      if (x == 1) {
+        if (y == 1) {
+          cells[id] = new Cell(id, directions.down, directions.right, null, null);
+        } else if (y == finalY) {
+          cells[id] = new Cell(id, directions.up, directions.right, null, null);
+        } else {
+          cells[id] = new Cell(id, directions.up, directions.right, directions.down, null);
+        }
+      }
 
+      else if (x == finalX) {
+        if (y == 1) {
+          cells[id] = new Cell(id, directions.left, directions.down, null, null);
+        } else if (y == finalY) {
+          cells[id] = new Cell(id, directions.left, directions.up, null, null);
+        } else {
+          cells[id] = new Cell(id, directions.up, directions.left, directions.down, null);
+        }
+      }
+
+      else if (y == 1) {
+        if (x == 1) {
+          cells[id] = new Cell(id, directions.down, directions.right, null, null);
+        } else if (x == finalX) {
+          cells[id] = new Cell(id, directions.left, directions.down, null, null);
+        } else {
+          cells[id] = new Cell(id, directions.left, directions.down, directions.right, null);
+        }
+      }
+
+      else if (y == finalY) {
+        if (x == 1) {
+          cells[id] = new Cell(id, directions.up, directions.right, null, null);
+        } else if (x == finalX) {
+          cells[id] = new Cell(id, directions.up, directions.left, null, null);
+        } else {
+          cells[id] = new Cell(id, directions.up, directions.left, directions.right, null);
+        }
+      }
+
+      else {
+        cells[id] = new Cell(id, directions.up, directions.down, directions.left, directions.right);
+      } 
+    } // second loop (y - height)
+  } // end first loop (x - width)
 }
 
 // global variables
@@ -20,11 +64,11 @@ let cell_id = 3
 let cells = {}; // hold all the cells in the maze
 cellSize = 25
 
-const neighbours = {
-  UP: "UP",
-  DOWN: "DOWN",
-  LEFT: "LEFT",
-  RIGHT: "RIGHT"
+const directions = {
+  up: "UP",
+  down: "DOWN",
+  left: "LEFT",
+  right: "RIGHT"
 }
 
 function draw() {
@@ -45,28 +89,28 @@ function draw() {
 }
 
 class Cell{
-  constructor(id, up, down, left, right){
+  constructor(id, d1, d2, d3, d4){
     this.id = id
 
-    if (arguments.includes(directions.UP)) {
+    if (arguments.includes(directions.up)) {
       this.up = cells[this.id - width]
     } else {
       this.up = null
     }
 
-    if(arguments.includes(directions.DOWN)) {
+    if(arguments.includes(directions.down)) {
       this.down = cells[this.id + width]
     } else {
       this.down = null
     }
 
-    if(arguments.includes(directions.LEFT)) {
+    if(arguments.includes(directions.left)) {
       this.left = cells[this.id - 1]
     } else {
       this.left = null
     }
 
-    if(arguments.includes(directions.RIGHT)) {
+    if(arguments.includes(directions.right)) {
       this.right = cells[this.id + 1]
     } else {
       this.right = null
