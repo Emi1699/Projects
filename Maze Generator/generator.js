2b805f843bc050100c721dc2d55abb4be3fd8b99
Emi1699 <emisv_16@yahoo.com>
2022-07-22 19:35:44 +0300

    added enum for direction (up,down,left,right) for Cell's neighbours

--- Maze Generator/generator.js
+++ Maze Generator/generator.js
@@ -1,44 +1,75 @@
 function setup() {
   createCanvas(windowWidth - 20, 650);
 
-  console.log(height)
-  console.log(width)
+  console.log(width);
+  console.log(height);
 
-  console.log("QWerqwer")
+   // create the cells in the maze
+  for (x = 1; x<= width / cellSize; x++) {
+    for (y = 1; y<= height / cellSize; y++) {
+      id = x + (width * (y - 1))
+
+      
+    }
+  }
+
 }
 
-let verr = 3
+// global variables
+let cell_id = 3
+let cells = {}; // hold all the cells in the maze
+cellSize = 25
+
+const neighbours = {
+  UP: "UP",
+  DOWN: "DOWN",
+  LEFT: "LEFT",
+  RIGHT: "RIGHT"
+}
 
 function draw() {
   background(25); // 0 = black, 255 = white
 
-  // global variables
-  cellWidth = 25
   stroke('yellow');
   strokeWeight(1.25)
 
   // draw vertical walls
-  for (i = 0; i <= width; i+= cellWidth) {
+  for (i = 0; i <= width; i+= cellSize) {
     line(i, 0, i, height)
   }
 
   // draw horizontal walls
-
-  for (j = 0; j <= width; j+= cellWidth) {
+  for (j = 0; j <= width; j+= cellSize) {
     line(0, j, width, j)
   }
-
 }
 
-cells = []
-
 class Cell{
-  constructor(width, height){
-    this.width = width;
-    this.height = height;
-  }
+  constructor(id, up, down, left, right){
+    this.id = id
 
-  draw() {
+    if (arguments.includes(directions.UP)) {
+      this.up = cells[this.id - width]
+    } else {
+      this.up = null
+    }
 
+    if(arguments.includes(directions.DOWN)) {
+      this.down = cells[this.id + width]
+    } else {
+      this.down = null
+    }
+
+    if(arguments.includes(directions.LEFT)) {
+      this.left = cells[this.id - 1]
+    } else {
+      this.left = null
+    }
+
+    if(arguments.includes(directions.RIGHT)) {
+      this.right = cells[this.id + 1]
+    } else {
+      this.right = null
+    }
   }
 }
