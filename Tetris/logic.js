a560eda3834162e47f279d80e718590e46fe62a0
Emi Buliga <emibuliga@Emis-MacBook-Pro.local>
2022-07-26 23:42:27 +0300

    changed background color

--- Tetris/logic.js
+++ Tetris/logic.js
@@ -1,7 +1,14 @@
-unction setup() {
-  createCanvas(600, 800);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   frameRate(144);
+
+
+
 }
 
 function draw() {
-  background(backgroundColour); // 0 = black, 255 = white
+  background(41, 21, 77); // 0 = black, 255 = white
+
+
+
+}
